92bf8ec368d6e8141c007f0b162d443fb68c4b5b
jean-mathieu vermosen <vermosen@pc-77.home>
2023-04-12 07:13:50 +0200

    up

--- src/bin/cmdargs.rs
+++ src/bin/cmdargs.rs
@@ -1,5 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-  // a simple program to capute command line args
-  let _i: i64 = 0;
-  println!("Hello World!");
+
+  let args = Cli::parse();
+
+  exit(0);
 }
